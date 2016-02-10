44f76ac6e072a047a14615c86e1d4059c3fa39d6
NickOosterhuis <nickoosterhuis@gmail.com>
2016-02-10 21:15:23 +0100

    Hex calculator voor opdracht 9

---
+++ Week 1/src/Hex/HexCalculator.java
@@ -0,0 +1,14 @@
+
+package Hex;
+
+public class HexCalculator { 
+    
+    public static void main(String[] args) 
+    {
+        long decimal = 3405691582L; 
+
+        System.out.println(Long.toHexString(decimal));
+    }
+}
+
+
