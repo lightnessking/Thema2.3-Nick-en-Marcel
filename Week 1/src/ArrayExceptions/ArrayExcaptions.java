5981a244d43f6ec15360d294ccc8a0e86feb499c
NickOosterhuis <nickoosterhuis@gmail.com>
2016-02-12 11:53:11 +0100

    opdracht 10 af

--- Week 1/src/ArrayExceptions/ArrayExcaptions.java
+++ Week 1/src/ArrayExceptions/ArrayExcaptions.java
@@ -4,27 +4,28 @@ package ArrayExceptions;
 
 public class ArrayExcaptions {
     
-    public void sumArrays() {
-        int[] eersteArray = {1, 2, 3, 4, 5}; 
-        int[] tweedeArray = {1, 2, 3, 4, 5}; 
-        
-        int sum = 0; 
-        
-        if(eersteArray.length == tweedeArray.length) {
-            for(int number : eersteArray) {
-                sum = sum + number; 
+    public static void sumArrays(int[] array1, int[] array2) throws ArrayMismatchException{
+            if(array1.length == array2.length) {
+                int[] array3 = new int[array1.length];
+                for(int number : array1) {
+                    array3[number - 1] = array1[number - 1] + array2[number - 1];
+                    System.out.println("The new array contains: " + array3[number - 1] + " at index number: " + (number-1));
+                }            
+            }else{
+                throw new ArrayMismatchException("Array length mismatch!");
             }
-
-            for(int number : tweedeArray) {
-                sum = sum + number;
-            }
-
-            System.out.println("Sum of both arrays = " + sum);
-        }
-        else {
-            System.out.println("Error");
-        }
     }
     
-    //implement custom try and catch ff opzoeken alles! 
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5}; 
+        int[] array2 = {1, 2, 3, 4}; 
+        
+        try{
+            sumArrays(array1, array2);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    } 
 }
+
+
